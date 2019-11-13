3792035e2db86e093fd51a8ee2e9755c44945465
Alan Mitchell <tabb99@gmail.com>
2019-11-12 23:58:51 -0900

    Structure to main routine.

---
+++ bmonreporter/config_logging.py
@@ -0,0 +1,36 @@
+"""Module used to configure exception and other application logging.
+"""
+import logging
+import logging.handlers
+import sys
+
+def configure_logging(logging_module, log_file_path, log_level):
+    """Configures the logging for an application.  Usage example:
+           import logging
+           import config_logging
+           config_logging.configure_logging(logging, '/var/log/logfilename', 'INFO')
+
+       Note that the 'log_level' parameter should be string matching a logging level
+       attribute of the logging module: e.g. "INFO" or "DEBUG"
+    """
+
+    # Use the root logger for the application.
+
+    # set the log level. Because we are setting this on the logger, it will apply
+    # to all handlers (unless maybe you set a specific level on a handler?).
+    logging_module.root.setLevel(getattr(logging, log_level))
+
+    # create a rotating file handler
+    fh = logging.handlers.RotatingFileHandler(log_file_path, maxBytes=200000, backupCount=5)
+
+    # create formatter and add it to the handler
+    formatter = logging_module.Formatter('%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+    fh.setFormatter(formatter)
+
+    # create a handler that will print to console as well.
+    console_h = logging_module.StreamHandler()
+    console_h.setFormatter(formatter)
+
+    # add the handlers to the root logger
+    logging_module.root.addHandler(fh)
+    logging_module.root.addHandler(console_h)
--- bmonreporter/reporter.py
+++ bmonreporter/reporter.py
@@ -2,14 +2,24 @@
 the processing logic and final display.
 """
 
+import sys
+import logging
+import tempfile
+from path import Path
+from urllib.parse import urlparse
+
 import boto3
 import papermill       # installed with: pip install papermill[s3], to include S3 IO features.
 import scrapbook       # install with: pip install nteract-scrapbook[s3], just in case S3 features used.
+
+from bmonreporter.file_util import copy_dir_tree
+import bmonreporter.config_logging
 
 def create_reports(
     template_path,      
     output_path,
     bmon_urls,
+    log_level,
     log_file_path='bmon-reporter-logs/',
     ):
     """Creates all of the reports for Organizations a Buildings across all specified BMON
@@ -29,4 +39,46 @@ def create_reports(
     print(f'''template: {template_path}
 output: {output_path}
 BMON URLs: {bmon_urls}
+Log Level: {log_level}
 Log File: {log_file_path}''')
+
+    # set up logging
+    # temporary directory for log files
+    log_dir = tempfile.TemporaryDirectory()
+    bmonreporter.config_logging.configure_logging(
+        logging, 
+        Path(log_dir.name) / 'bmonreporter.log', 
+        log_level
+    )
+
+    try:
+        # temporary directory for report templates
+        templ_dir = tempfile.TemporaryDirectory()
+        print(templ_dir.name)
+        # copy the report templates into this directory
+        copy_dir_tree(template_path, templ_dir.name)
+
+        # Loop through the BMON servers to process
+        for server_url in bmon_urls:
+            try:
+                # create a temporary directory to write reports
+                report_dir = tempfile.TemporaryDirectory()
+                with open(Path(report_dir.name) / 'test.html', 'w') as fout:
+                    fout.write('hello')
+
+            except:
+                logging.exception(f'Error processing server {server_url}')
+
+            finally:
+                # copy the report files to their final location
+                copy_dir_tree(
+                    report_dir.name, 
+                    str(Path(output_path) / urlparse(server_url).netloc)
+                )
+
+    except:
+        logging.exception('Error setting up reporter.')
+
+    finally:
+        # copy the temporary logging directory to its final location
+        copy_dir_tree(log_dir.name, log_file_path)
